b52aa5d06a6e051db3eed4c6ac16dceaac28f7eb
MacCracken <maccracken@Achaemenid.fios-router.home>
2017-10-10 09:50:49 -0700

    separated dice and randomint to its own file

---
+++ dice.js
@@ -0,0 +1,25 @@
+// Random Int Generator and Dice Types
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+module.exports = {
+    rollD20: function() {
+        return getRandomInt(1,20);
+    },
+    rollD12: function() {
+        return getRandomInt(1,12);
+    },
+    rollD10: function() {
+        return getRandomInt(1,10);
+    },
+    rollD8: function() {
+        return getRandomInt(1,8);
+    },
+    rollD6: function() {
+        return getRandomInt(1,6);
+    },
+    rollD4: function() {
+        return getRandomInt(1,4);
+    }
+}
--- generate.js
+++ generate.js
@@ -1,4 +1,5 @@
 // Returns a random integer between min (included) and max (included)
+const dice = require('./dice');
 var fs=require('fs');
 var ideals=JSON.parse(fs.readFileSync('ideals.json'));
 var chars=JSON.parse(fs.readFileSync('characteristics.json'));
@@ -7,19 +8,16 @@ var flaws=JSON.parse(fs.readFileSync('flaws.json'));
 var first=JSON.parse(fs.readFileSync('first.json'));
 var middle=JSON.parse(fs.readFileSync('middle.json'));
 var last=JSON.parse(fs.readFileSync('last.json'));
-var first_name = first[getRandomInt(1,20)] + middle[getRandomInt(1,20)] + last[getRandomInt(1,20)]
-var last_name = first[getRandomInt(1,20)] + middle[getRandomInt(1,20)] + last[getRandomInt(1,20)]
+var first_name = first[dice.rollD20()] + middle[dice.rollD20()] + last[dice.rollD20()];
+var last_name = first[dice.rollD20()] + middle[dice.rollD20()] + last[dice.rollD20()];
 
 var npc_character = new Object();
 npc_character["Name"] = first_name + " " + last_name;
-npc_character["Characteristics"] = chars[getRandomInt(1,20)];
-npc_character["Ideals"] = ideals[getRandomInt(1,20)];
-npc_character["Bonds"] = bonds[getRandomInt(1,10)];
-npc_character["Flaws"] = flaws[getRandomInt(1,12)];
+npc_character["Characteristics"] = chars[dice.rollD20()];
+npc_character["Ideals"] = ideals[dice.rollD20()];
+npc_character["Bonds"] = bonds[dice.rollD10()];
+npc_character["Flaws"] = flaws[dice.rollD12()];
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 console.log("--------------------------")
 console.log("D&D 5E NPC Generator v 1.0")
